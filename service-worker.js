8c04547bf3ba752041ecdcbaf5c935d55e7f613b
杨晨 <cyang@itekoo.com>
2019-02-26 10:51:32 +0800

    my blog

---
+++ service-worker.js
@@ -0,0 +1,108 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [
+  {
+    "url": "404.html",
+    "revision": "aa75112a15ab660cf46847e5b764d419"
+  },
+  {
+    "url": "about/index.html",
+    "revision": "dec16f63b9776356bbf53fbf7d3f0149"
+  },
+  {
+    "url": "assets/css/0.styles.559343f1.css",
+    "revision": "aab2df64dcc02cbddac2cac826c21dc8"
+  },
+  {
+    "url": "assets/img/search.83621669.svg",
+    "revision": "83621669651b9a3d4bf64d1a670ad856"
+  },
+  {
+    "url": "assets/js/2.a68dd455.js",
+    "revision": "6204369811a20bc0ad152cdea41e0496"
+  },
+  {
+    "url": "assets/js/3.2820c1a6.js",
+    "revision": "3f96f7ee834e2fc2109e34ba8d4051e7"
+  },
+  {
+    "url": "assets/js/4.5db0b1a2.js",
+    "revision": "5a1fe1e411fc845fced856a43ad17af0"
+  },
+  {
+    "url": "assets/js/5.4e2d8b59.js",
+    "revision": "7b2cdf61506a9e1e3b6ecd53dec68d1c"
+  },
+  {
+    "url": "assets/js/6.82bb8614.js",
+    "revision": "7d4a31e8a706e519b9da8924d393858e"
+  },
+  {
+    "url": "assets/js/7.8b0e4873.js",
+    "revision": "5f749548c91e5e2f08d65d5bb91bc3d3"
+  },
+  {
+    "url": "assets/js/8.41e79da7.js",
+    "revision": "75a864670292124cfb56ca862ed123b8"
+  },
+  {
+    "url": "assets/js/app.bcb92e6c.js",
+    "revision": "30d4e3e732b277b51ccd925eb9ae3adb"
+  },
+  {
+    "url": "blog/hello.html",
+    "revision": "a50ed4e5818cc22c5a823d7bd0b8d00a"
+  },
+  {
+    "url": "blog/test.html",
+    "revision": "6a74e131ded1d96f9870a324ed77d1de"
+  },
+  {
+    "url": "img/avatar.jpg",
+    "revision": "731c879d520d64819b19617737356248"
+  },
+  {
+    "url": "img/brand.jpg",
+    "revision": "6285e20c35c902b4514f80134ecec543"
+  },
+  {
+    "url": "img/logo.png",
+    "revision": "cf23526f451784ff137f161b8fe18d5a"
+  },
+  {
+    "url": "index.html",
+    "revision": "c99e63227163bfe539c0f6b7402ed46b"
+  }
+].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+addEventListener('message', event => {
+  const replyPort = event.ports[0]
+  const message = event.data
+  if (replyPort && message && message.type === 'skip-waiting') {
+    event.waitUntil(
+      self.skipWaiting().then(
+        () => replyPort.postMessage({ error: null }),
+        error => replyPort.postMessage({ error })
+      )
+    )
+  }
+})
